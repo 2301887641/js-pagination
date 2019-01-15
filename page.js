25a937512a19b43a76d0f0ab12a050d16afb92cd
隋国珍 <s15863549041@126.com>
2019-01-15 14:25:14 +0800

    init

---
+++ page.js
@@ -0,0 +1,22 @@
+function Pagination(config){
+
+
+
+
+
+}
+Pagination.config={
+    //当前页
+    current:1,
+    //总记录数
+    total:1,
+    //每页显示的数量
+    pageSize:10
+};
+Pagination.foundation={};
+Pagination.prototype={
+  construct:Pagination,
+  init:function(){
+
+  }
+};
